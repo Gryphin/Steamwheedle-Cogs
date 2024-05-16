d577844a36e8c9e4869598397984ef7eef24456c
Gryphin <Gryphin@gmail.com>
2024-05-16 13:39:22 -0700

    Update __init__.py

--- roster/__init__.py
+++ roster/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .recruit import recruit
+from .roster import roster
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
