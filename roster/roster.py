384308267bc6684de5637220bd943872c1e1affb
Gryphin <Gryphin@gmail.com>
2024-05-27 14:42:23 -0700

    Update roster.py

--- roster/roster.py
+++ roster/roster.py
@@ -32,19 +32,20 @@ class roster(commands.Cog):
         # TODO: Replace this with the proper end user data removal handling.
         super().red_delete_data_for_user(requester=requester, user_id=user_id)
 ###If PermissionError: [Errno 13] Permission denied: 'guild.txt' use absolute path to guild.txt
-        @app_commands.command()
-    async def roster(self, interaction: discord.Interaction):
+    @app_commands.command()
+    async def recruit(self, interaction: discord.Interaction):
         mypath = os.path.dirname(os.path.abspath(__file__))
         gc = gspread.service_account()
         sh = gc.open("Steamwheedle Recruitment")
         worksheet = sh.worksheet("Roster")
         roster_list = [item for item in worksheet.col_values(1) if item]
         original_stdout = sys.stdout
-        with open('roster.txt', 'w') as f:
+        with open('mypath+'/roster.txt'', 'w') as f:
             with redirect_stdout(f):
                 for item in roster_list:
                     print(item)
                 sys.stdout = original_stdout
-        with open('roster.txt', 'r') as g:
+        with open('mypath+'/roster.txt'', 'r') as g:
                 content = g.read()
-                await ctx.send(content)
+                embed = discord.Embed(title='Guild Listings', description=f"{content}", color=discord.Color.green())
+                await interaction.response.send_message(embed=embed)
