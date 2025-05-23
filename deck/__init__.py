e3bfb034b464dd07ef814fba7167c37a6e2f19b2
Gryphin <Gryphin@gmail.com>
2025-05-23 16:38:11 -0700

    Update __init__.py

--- deck/__init__.py
+++ deck/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .deck import RumbloDecoder
+from .deck import deck
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
