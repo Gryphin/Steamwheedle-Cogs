580b8eb2a9a18e40f091f0b92448fcd179e3867f
Gryphin <Gryphin@gmail.com>
2025-05-27 16:20:17 -0700

    Update deck.py

--- deck/deck.py
+++ deck/deck.py
@@ -1057,7 +1057,7 @@ def combine_unit_images(unit_names: List[str]) -> Optional[io.BytesIO]:
     embed.set_image(url="attachment://loadout.png")
 
                 # Send the embed with the combined image as a file
-                await ctx.send(embed=embed, file=discord.File(fp=image_binary, filename='loadout.png'))
+    await ctx.send(embed=embed, file=discord.File(fp=image_binary, filename='loadout.png'))
 
         else:
             await ctx.send("Failed to decode the Rumblo code. Please check the code's validity.")
